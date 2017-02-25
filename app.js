260fcf7992f638a3b22222dae6fff90a78f5f577
gioperalto <gpera008@fiu.edu>
2017-02-24 23:13:14 -0500

    Added mongoose; express; basic setup

---
+++ app.js
@@ -0,0 +1,37 @@
+// app.js
+
+// ---------------------------------------------------------
+// Setup
+// ---------------------------------------------------------
+
+// Declare variables
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var configDB     = require('./config/database');
+var port         = process.env.PORT || 1337;
+
+// Allow Express to serve static files in "public" directory
+app.use(express.static('public'));
+
+// Views + Templating
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// ---------------------------------------------------------
+// Database configurations
+// ---------------------------------------------------------
+mongoose.connect(configDB.url);
+
+// ---------------------------------------------------------
+// API Endpoints
+// ---------------------------------------------------------
+require('./config/api')(app);
+
+// ---------------------------------------------------------
+// Launch
+// ---------------------------------------------------------
+
+// Server launch (TODO: Implement try-catch)
+app.listen(port);
+console.log('Launched server at: http://localhost:' + port);
